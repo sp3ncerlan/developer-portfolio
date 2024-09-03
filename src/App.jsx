a6bccc7219d09090856740a3c5ec518e4a60cfb4
sp3ncerlan <lansp329@gmail.com>
2024-09-03 15:03:15 -0500

    Vercel react addition

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,7 @@ function App() {
       </button>
       <div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
         <div className="max-w-5xl w-11/12 mx-auto">
+          <SpeedInsights />
           <Intro />
           <Portfolio />
           <Timeline />

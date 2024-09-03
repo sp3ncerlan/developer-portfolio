880709317017fba89450bd272ac8c4c92ea8549a
sp3ncerlan <lansp329@gmail.com>
2024-09-03 15:28:21 -0500

    Changed commentary and title/labels

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl dark:text-white b-1 md:mb-3 font-bold">Spencer</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Fullstack Software Engineer</p>
+            <h1 className="text-4xl md:text-7xl dark:text-white b-1 md:mb-3 font-bold">Spencer Lan</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Aspiring Fullstack Software Engineer</p>
             <p className="text-sm max-w-xl mb-6 font-bold">With a background in Finance and a BBA from Texas A&M University, I have experience in credit analysis and portfolio management as a Senior Analyst at Citi.
             </p>
 
             <p className="text-sm max-w-xl mb-6 font-bold">
-            Now, I'm pursuing a BS in Computer Science at Oregon State University - building skills in software development and problem solving. I am actively seeking opportunities to transition into a role in software, where I aim to hone my developmental toolbelt and learn from the best in the industry.
+            Currently, I'm pursuing a BS in Computer Science at Oregon State University - building skills in software development and problem solving. I am actively seeking opportunities to transition into a role in software, where I aim to hone my developmental toolbelt and learn from the best in the industry.
             </p>
         </div>
     )
